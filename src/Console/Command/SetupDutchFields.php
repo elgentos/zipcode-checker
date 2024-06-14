a4cbb38f20d99bab8b7ccc5fa13e3b19be213d32
Michel <michel@elgentos.nl>
2024-06-14 15:31:24 +0200

    Added command to update hyva settings

---
+++ src/Console/Command/SetupDutchFields.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Copyright Elgentos BV. All rights reserved.
+ * https://www.elgentos.nl/
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+
+class SetupDutchFields extends Command
+{
+    protected array $changes = [
+        [
+            'path' => 'hyva_themes_checkout/address_form/street/field_label_0',
+            'value' => 'Street'
+        ],
+        [
+            'path' => 'hyva_themes_checkout/address_form/street/field_label_1',
+            'value' => 'Housenumber'
+        ]
+    ];
+
+    public function __construct(
+        private WriterInterface $configWriter,
+        ?string $name = null
+    ){
+        parent::__construct($name);
+    }
+
+    private const NAME = 'name';
+
+    protected function configure(): void
+    {
+        $this->setName('elgentos:checkout:setup-dutch-fields');
+        $this->setDescription('Change default setting of Hyva to Dutch form setup.');
+
+        parent::configure();
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        try {
+
+            foreach ($this->changes as $change) {
+                $this->configWriter->save(
+                    $change['path'],
+                    $change['value'],
+                    ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+                    $scopeId = 0
+                );
+            }
+        }
+        catch (Exception) {
+            return 0;
+        }
+
+
+        return 1;
+    }
+
+
+}
